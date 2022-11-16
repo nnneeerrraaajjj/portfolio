522a21942390a2c84117fc68ae9cb4697f660115
nnneeerrraaajjj <36910326+nnneeerrraaajjj@users.noreply.github.com>
2022-11-16 15:07:11 +0530

    update resume link

--- src/root.link.js
+++ src/root.link.js
@@ -1,4 +1,4 @@
-export const RESUME_PATH = 'https://drive.google.com/file/d/1N0vk68b-IIa2FsOxXoCTIj7f-qMOezab/view?usp=sharing';
+export const RESUME_PATH = 'https://drive.google.com/file/d/1wrv0KA4VBS6xHP2iDQuWZOmymTj_4Gsj/view?usp=share_link';
 export const GITHUB_USERNAME = 'nnneeerrraaajjj';
 export const LINKEDIN_USERNAME = 'nnneeerrraaajjj';
 export const FIRST_NAME = 'Neerajprasad';
@@ -6,5 +6,5 @@ export const LAST_NAME = 'Singh';
 export const ADDRESS = 'Mumbai'
 export const EMAIL_ID = 'singh.neerajprasad3@gmail.com';
 export const FRONT_PAGE_NAME = 'Neerajprasad Singh';
-export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
+export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor. I love to build things for web and solving problems.'
 
